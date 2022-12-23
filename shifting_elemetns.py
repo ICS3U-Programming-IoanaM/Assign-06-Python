62e4e32120213140a7655ad4d78fabb0b114d42f
Ioana Marinescu <sh ./setup.shioana.marinescu@stu.ocsb.ca>
2022-12-23 22:30:48 +0000

    user can now rerun the program if they want to

--- shifting_elemetns.py
+++ shifting_elemetns.py
@@ -6,6 +6,21 @@
 # shifts the elements in a list x times in a specified direction
 
 
+def rerun():
+    print()
+    while (True):
+        # asks user if they want to rerun the program
+        play_again = input("Do you want to rerun the program? (y/n) ")
+
+        if play_again == "y" or play_again == "Y":
+            return True
+
+        if play_again == "n" or play_again == "N":
+            return False
+
+        print("Please type either y or n.")
+
+
 def shift_list(nums_list, shifts):
     # variables
     list_length = len(nums_list) - 1
@@ -39,44 +54,50 @@ def opening_message():
 # main gets user input and runs all the other functions
 def main():
     # variables
-    list_of_inputs = []
-    final_list = []
+    go_again = True
 
     # opening message
     opening_message()
 
-    # getting user input for each number
-    while(True):
-        user_input = input("Enter a number you want in your list (q to quit): ")
+    while(go_again):
+        # lists
+        list_of_inputs = []
+        final_list = []
 
-        # user doesn't want to enter more numbers
-        if (user_input == "/q"):
-            break
+        # getting user input for each number
+        while(True):
+            user_input = input("Enter a number you want in your list (q to quit): ")
 
-        # adds input to list
-        list_of_inputs.append(user_input)
+            # user doesn't want to enter more numbers
+            if (user_input == "/q"):
+                break
 
-    # displays the list
-    print(f"Here is your list: {list_of_inputs}")
+            # adds input to list
+            list_of_inputs.append(user_input)
 
-    # gets user input for how many elements to the left the user wants to shift to the left
-    shifted_elements_str = input("How many elements do you want to shift to the left? ")
+        # displays the list
+        print(f"Here is your list: {list_of_inputs}")
 
-    # exception handling for second set of input
-    try:
-        shifted_elements_int = int(shifted_elements_str)
+        # gets user input for how many elements to the left the user wants to shift to the left
+        shifted_elements_str = input("How many elements do you want to shift to the left? ")
 
-    # user input is not an integer
-    except Exception:
-        print("Please make sure your input is an integer!")
+        # exception handling for second set of input
+        try:
+            shifted_elements_int = int(shifted_elements_str)
 
-    # user input is valid
-    else:
-        # calls function to shit the elements of the list
-        final_list = shift_list(list_of_inputs, shifted_elements_int)
+        # user input is not an integer
+        except Exception:
+            print("Please make sure your input is an integer!")
+            go_again = rerun()
 
-        # final message
-        print(f"The list shifted {shifted_elements_int} elements to the left is {final_list}")
+        # user input is valid
+        else:
+            # calls function to shit the elements of the list
+            final_list = shift_list(list_of_inputs, shifted_elements_int)
+
+            # final message
+            print(f"The list shifted {shifted_elements_int} elements to the left is {final_list}")
+            go_again = rerun()
 
 
 # runs main
